9957d52e12eab1a4b2aa1ff20e24facb73d0b09e
Gabriel Pratt <gpratt@ucsd.edu>
2013-06-21 01:51:49 -0700

    fixed a count aligned bug and comitting half working a clipseq analysis for local testing

--- general/count_aligned_from_sam.py
+++ general/count_aligned_from_sam.py
@@ -8,19 +8,23 @@ import argparse
 from collections import Counter
 import sys
 
-parser = argparse.ArgumentParser(
-    description='Counts Counts Chromosomes in sam file (used generally for getting counts of rRNA elements after filtering them), writes results to stdout')
-parser.add_argument(
-    '--input', 
-    help='input sam file', default=sys.stdin)
+parser = argparse.ArgumentParser(description='Counts Counts Chromosomes in sam file (used generally for getting counts of rRNA elements after filtering them), writes results to stdout')
+parser.add_argument('--input', help='input sam file', default=sys.stdin)
 
 args = parser.parse_args()
 counts = Counter()
 
-for line in args.input:
+if args.input != sys.stdin:
+    handle = open(args.input)
+else:
+    handle = args.input
+
+for line in handle:
     line = line.strip().split()
-    if line[2] == "*" or line[0].startswith("@"):
+
+    if line[0].startswith("@") or line[2] == "*":
         continue
+    
 
     counts[line[2]] += 1
 
