3bf7ed436cfbf510b9fba0b017483ebac8c7972e
spikesd17 <michaeltlovci@gmail.com>
2013-07-04 10:33:12 -0700

    directory structure cleanup so "import gscripts" now works

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup, find_packages
 
+scripts=['rnaseq/count_tags.py',
+         'rnaseq/single_RPKM.py',
+         'general/calculate_NRF.py',
+         'general/calculate_NRF.py',
+         'general/make_trackhubs.py',
+         'general/count_aligned_from_sam.py',
+         'general/negBedGraph.py',
+         'clipseq/perform_idr.py',
+         'clipseq/run_piranha.py',
+         'riboseq/riboseq_coverage.py']
+scripts = map((lambda x: "gscripts/" + x), scripts)
+
+
 with open("README") as file:
     long_description = file.read()
 
@@ -21,16 +34,7 @@ setup(
                         ],
       
     setup_requires = ["setuptools_git >= 0.3",],
-    scripts=['rnaseq/count_tags.py',
-             'rnaseq/single_RPKM.py',
-             'general/calculate_NRF.py',
-             'general/calculate_NRF.py',
-             'general/make_trackhubs.py',
-             'general/count_aligned_from_sam.py',
-             'general/negBedGraph.py',
-             'clipseq/perform_idr.py',
-             'clipseq/run_piranha.py',
-             'riboseq/riboseq_coverage.py'],
+    scripts=scripts,
 
     #metadata for upload to PyPI
     author = "Gabriel Pratt",
