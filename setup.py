03adcd27fefd5d5d8938d491143fce602d227497
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-07-11 08:17:21 -0700

    missing comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 scripts=['rnaseq/count_tags.py',
          'rnaseq/single_RPKM.py',
-         'rnaseq/submit_miso_index_gtf.py'
+         'rnaseq/submit_miso_index_gtf.py',
          'general/calculate_NRF.py',
          'general/calculate_NRF.py',
          'general/make_trackhubs.py',
