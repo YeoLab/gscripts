cc6ba4cbbd36f25edc52e1c85e437f965ca6a72b
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-07-15 11:54:06 -0700

    fixed which import

--- gscripts/miso/submit_miso_pipeline.py
+++ gscripts/miso/submit_miso_pipeline.py
@@ -6,7 +6,7 @@ import argparse
 
 # Submit jobs to the cluster
 from gscripts.general.qtools import Submitter
-from gscripts import which
+import gscripts
 
 # File name manipulations
 import os
@@ -135,7 +135,7 @@ def main():
         sample_info_file = cl.args['sample_info_file']
 
         try:
-            miso_scripts_dir = os.dirname(which('miso')[0])
+            miso_scripts_dir = os.dirname(gscripts.which('miso')[0])
         except IndexError:
             # If there is an IndexError, that means that 'which' returned an
             # empty list, and thus there is no miso installed on the path.
