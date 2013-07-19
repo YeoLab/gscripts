b2c89e8c49a6fa62812852fde6025b10d59e4bc6
Michael Lovci <michaeltlovci@gmail.com>
2013-07-19 16:36:26 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ scripts=['rnaseq/count_tags.py',
          'general/negBedGraph.py',
          'clipseq/perform_idr.py',
          'clipseq/run_piranha.py',
-         'riboseq/riboseq_coverage.py']
+         'riboseq/riboseq_coverage.py',
+	 'rnaseq/oldsplice.py',
+	 'rnaseq/compare_oldsplice.py',
+	 'output_parsers/parseMiso.py'	]
 scripts = map((lambda x: "gscripts/" + x), scripts)
 
 
-with open("README") as file:
+with open("README.rst") as file:
     long_description = file.read()
 
 setup(
@@ -34,20 +37,7 @@ setup(
                         ],
       
     setup_requires = ["setuptools_git >= 0.3",],
-<<<<<<< HEAD
     scripts=scripts,
-=======
-    scripts=['rnaseq/count_tags.py',
-             'rnaseq/single_RPKM.py',
-             'general/calculate_NRF.py',
-             'general/calculate_NRF.py',
-             'general/make_trackhubs.py',
-             'general/count_aligned_from_sam.py',
-             'general/negBedGraph.py',
-             'clipseq/perform_idr.py',
-             'clipseq/run_piranha.py',
-             'riboseq/riboseq_coverage.py'],
->>>>>>> updated qscripts and assocated scripts to work only on tscc
 
     #metadata for upload to PyPI
     author = "Gabriel Pratt",
