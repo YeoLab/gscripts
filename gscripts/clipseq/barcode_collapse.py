6984a0c1ded2c87bf5e04b93eab593fbbd15c9e7
Gabriel Pratt <gpratt@ucsd.edu>
2013-07-30 15:08:43 -0700

    updated barcode collapse to record slighlty better metrics

--- gscripts/clipseq/barcode_collapse.py
+++ gscripts/clipseq/barcode_collapse.py
@@ -36,8 +36,10 @@ def barcode_collapse(inBam, outBam, barcoded, use_stop):
     outBam = pysam.Samfile(outBam, 'wb', template=inBam)
     
 
-    cur_count = 0
-    prev_chrom = None
+(options,args) = parser.parse_args()
+
+if not (options.bam.endswith(".bam")):
+    raise TypeError
 
     if use_stop:
         prev_pos = (0, 0)
@@ -70,54 +72,36 @@ def barcode_collapse(inBam, outBam, barcoded, use_stop):
                 outTotal.write(var_step)
                 outBarcodes.write(var_step)
 
-            out_pos = prev_pos[0] if use_stop else prev_pos
-            barcode_counts = np.array(barcode_set.values())
-            barcode_probablity = barcode_counts / float(sum(barcode_counts))
-            entropy = -1 * sum(barcode_probablity * np.log2(barcode_probablity))
-            
-            outEntropy.write("\t".join(map(str, [out_pos, entropy])) + "\n")
-            outTotal.write("\t".join(map(str, [out_pos, cur_count])) + "\n")
-            outBarcodes.write("\t".join(map(str, [out_pos, len(barcode_set)])) + "\n")
-            barcode_set = Counter()
-            cur_count = 0 
-
+#want total number of reads assocated with each barcode
+#number of reads removed assocated with each barcode
+#some sort of histogram measure of where duplicates are being removed from the most
+#ideally this would be a wiggle track, but for now I'll just keep track of a histogram / before and after trick
+barcode_set = set([])
+removed_count = Counter()
+total_count = Counter()
+for i, read in enumerate(inBam.fetch()):
+    #if we advance a position, reset barcode counting
+    if not read.pos == prev_pos:
+        barcode_set = set([])
         
-        total_count[barcode] += 1
-        if barcode in barcode_set:
-            removed_count[barcode] += 1
-        else:
-            outBam.write(read)
+    barcode = read.qname[:9]
+    
+    total_count[barcode] += 1
+    if barcode in barcode_set:
+        removed_count[barcode] += 1
+    else:
+        outBam.write(read)
+        
+    barcode_set.add(barcode)    
+    prev_pos = read.pos
 
-            
-        barcode_set[barcode] += 1   
-        prev_pos = cur_pos
-        prev_chrom = cur_chrom
-
-    out_pos = prev_pos[0] if use_stop else prev_pos
-    outTotal.write("\t".join(map(str, [out_pos, cur_count])) + "\n")
-    outBarcodes.write("\t".join(map(str, [out_pos, len(barcode_set)])) + "\n")
-    
-    inBam.close()
-    outBam.close()
-    
-    outEntropy.close()
-    outTotal.close()
-    outBarcodes.close()
-    return total_count, removed_count
-
-if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-b", "--bam", dest="bam", help="bam file to barcode collapse")
-    parser.add_option("-c", "--randomer", action="store_true", dest="barcoded", help="bam files are iclip barcoded")
-    parser.add_option("-s", "--use_stop", action="store_true", dest="use_stop", help="use stop as well as start to figure collapse")
-    parser.add_option("-o", "--out_file", dest="out_file")
-    parser.add_option("-m", "--metrics_file", dest="metrics_file")
-    
-    
-    (options,args) = parser.parse_args()
-    
-    if not (options.bam.endswith(".bam")):
-        raise TypeError("%s, not bam file" % (options.bam))
+inBam.close()
+outBam.close()
+        
+with open(options.metrics_file, 'w') as metrics:
+    metrics.write("\t".join(["total_count", "removed_count"]) + "\n")
+    for barcode in total_count.keys():
+        metrics.write("\t".join(map(str, [barcode, total_count[barcode], removed_count[barcode]])) + "\n")
 
     pysam.index(options.bam)
     total_count, removed_count = barcode_collapse(options.bam, 
