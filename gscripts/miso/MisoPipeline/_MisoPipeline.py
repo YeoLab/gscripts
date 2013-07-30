70728885b166e4ab24cae3e1666d899f2170406b
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-07-30 11:47:40 -0700

    MisoPipeline test 7, edited submission script name of summary and psi

--- gscripts/miso/MisoPipeline/_MisoPipeline.py
+++ gscripts/miso/MisoPipeline/_MisoPipeline.py
@@ -241,8 +241,9 @@ class MisoPipeline(object):
             psi_commands.append(psi_command)
 
         # Put the submitter script wherever the command was run from
-        submit_sh = self.submit_sh_suffix if self.submit_sh_suffix is\
-            not None else 'miso_%s.sh' % self.event_type
+        submit_sh = 'miso_%s_psi_%s.sh' % (self.submit_sh_suffix,
+                                         self.event_type)
+
         job_name = 'miso_%s_%s_psi' % (self.submit_sh_suffix, self.event_type)
         sub = Submitter(queue_type='PBS', sh_file=submit_sh,
                         command_list=psi_commands, job_name=job_name)
@@ -277,8 +278,8 @@ class MisoPipeline(object):
             summary_commands.append(summary_command)
         
         # Put the submitter script wherever the command was run from
-        submit_sh = self.submit_sh_suffix if self.submit_sh_suffix is\
-            not None else 'miso_%s.sh' % self.event_type
+        submit_sh = 'miso_%s_summary_%s.sh' % (self.submit_sh_suffix,
+                                         self.event_type)
         job_name = 'miso_%s_%s_summary' % (self.submit_sh_suffix,
                                            self.event_type)
         if self.psi_pbs_id is not None:
