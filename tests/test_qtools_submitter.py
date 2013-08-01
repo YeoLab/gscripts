cf370c86385e307be0b9753a39e0562b0ddb9fab
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-08-01 10:23:50 -0700

    gscripts.qtools.Submitter tests: testing (testing tests? HOW META). added wait_for_array test

--- tests/test_qtools_submitter.py
+++ tests/test_qtools_submitter.py
@@ -29,6 +29,8 @@ class Test(unittest.TestCase):
 #PBS -l nodes=1:ppn=16
 #PBS -A yeo-group
 #PBS -q home-yeo
+
+# Go to the directory from which the script was called
 cd $PBS_O_WORKDIR
 date
 echo testing PBS''' % (tests.get_test_dir(), tests.get_test_dir())
@@ -62,6 +64,45 @@ echo testing PBS''' % (tests.get_test_dir(), tests.get_test_dir())
 #PBS -l nodes=1:ppn=16
 #PBS -A yeo-group
 #PBS -q home-yeo
+#PBS -W depend=afterok:11111
+
+# Go to the directory from which the script was called
+cd $PBS_O_WORKDIR
+date
+echo testing PBS''' % (tests.get_test_dir(), tests.get_test_dir())
+        true_result = true_result_string.split('\n')
+
+        with open(submit_sh) as f:
+            for x in f.readlines():
+                print x,
+
+        for true, test in zip(true_result, open(submit_sh)):
+            self.assertEqual(true.strip().split(), test.strip().split())
+
+        # Make sure the job ID is a single (potentially multi-digit) integer
+        self.assertRegexpMatches(job_id, '^\d+$')
+
+    def test_wait_for_array_pbs(self):
+        commands = ['date', 'echo testing PBS']
+        job_name = 'test_qtools_submitter_wait_for_pbs'
+        submit_sh = '%s/%s.sh' % (tests.get_test_dir(), job_name)
+        sub = Submitter(queue_type='PBS', sh_file= submit_sh,
+                        command_list=commands,
+                        job_name=job_name, wait_for_array=['11111'])
+        job_id = sub.write_sh(submit=True, nodes=1, ppn=16,
+                                 queue='home-yeo', walltime='0:01:00')
+        true_result_string = '''#!/bin/sh
+#PBS -N test_qtools_submitter_wait_for_pbs
+#PBS -o %s/test_qtools_submitter_wait_for_pbs.sh.out
+#PBS -e %s/test_qtools_submitter_wait_for_pbs.sh.err
+#PBS -V
+#PBS -l walltime=0:01:00
+#PBS -l nodes=1:ppn=16
+#PBS -A yeo-group
+#PBS -q home-yeo
+#PBS -W depend=afterarrayok:11111
+
+# Go to the directory from which the script was called
 cd $PBS_O_WORKDIR
 date
 echo testing PBS''' % (tests.get_test_dir(), tests.get_test_dir())
