f31c2711ca3437790d062814ab9c5dfb1a20a8ed
spikesd17 <michaeltlovci@gmail.com>
2013-08-21 15:49:42 -0700

    fixed typo

--- gscripts/rnaseq/compare_oldsplice.py
+++ gscripts/rnaseq/compare_oldsplice.py
@@ -48,7 +48,7 @@ def main(options):
             sample_label = sample[1]
             with open(sample_label + ".oldsplice.SE", 'w') as SEout:
                 for gene in spliceData["SE"]:
-                    for loc in spliceData["SE"]['gene']:
+                    for loc in spliceData["SE"][gene]:
                         sample_IN = spliceData["SE"][gene][loc][sample_label]["IN"]
                         sample_EX = spliceData["SE"][gene][loc][sample_label]["EX"]
                         psi = ((sample_IN +2.) / 2) / (((sample_IN +2.) / 2) + (sample_EX+1))
@@ -68,7 +68,7 @@ def main(options):
         
             with open(sample_label + ".oldsplice.MXE", 'w') as MXEout:
                 for gene in spliceData["MXE"]:
-                    for loc in spliceData["MXE"]['gene']:
+                    for loc in spliceData["MXE"][gene]:
                         sample_B = spliceData["MXE"][gene][loc][sample_label]["A"]
                         sample_B = spliceData["MXE"][gene][loc][sample_label]["B"]
                         psi = (sample_A / (sample_A + sample_B)) #percent of reads representing isoform A
