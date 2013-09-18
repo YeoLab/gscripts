32c44988b1b134bd2a464b7ba85f2bf892252c33
mlovci <michaeltlovci@gmail.com>
2013-09-18 14:05:22 -0700

    updated run_oldsplice.py to use splice_type parameter

--- gscripts/rnaseq/run_oldsplice.py
+++ gscripts/rnaseq/run_oldsplice.py
@@ -13,8 +13,8 @@ for row, dat in sInfo.iterrows():
     id =  dat['Sample ID']
     bam = dat['Bam File']
     out = id + ".splices"
-    oldsplice_command = "oldsplice.py -b %s -s hg19 -o %s --splicetypes SE --splicetypes MXE --processors 16" %(bam, out)
+    oldsplice_command = "oldsplice.py -b %s -s hg19 -o %s --splice_type SE --splice_type MXE --processors 16" %(bam, out)
     cmds.append(oldsplice_command)
-    oldsplice_command = "oldsplice.py -f -b %s -s hg19 -o %s --splicetypes SE --splicetypes MXE --processors 16" %(bam, out.replace(".splices", ".flip.splices")  )
+    oldsplice_command = "oldsplice.py -f -b %s -s hg19 -o %s --splice_type SE --splice_type MXE --processors 16" %(bam, out.replace(".splices", ".flip.splices")  )
     cmds.append(oldsplice_command)
 Sub.job(command_list=cmds, sh_file="runOldsplice.sh", job_name="oldsplice", array=True, queue="home", nodes=1, ppn=16, submit=True)
