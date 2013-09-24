0eda879bc2f6ae35c14a8513965c234ac7dce0da
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-09-24 11:36:34 -0700

    Updated PCA to be more user friendly
    Added documentation for plot_pca
    
    fixing column_ids_dict in plot_pca
    
    fixed documentation error in plot_pca
    
    removed size_scale in plot_pca because text is way too small
    
    changed log_final_out tou output a real percentage instead of a fraction
    
    added gscripts.math and binify(), KLD, and JSD for dataframes

--- gscripts/miso/MisoPipeline/_MisoPipeline.py
+++ gscripts/miso/MisoPipeline/_MisoPipeline.py
@@ -260,8 +260,8 @@ class MisoPipeline(object):
             # output_dir = '%s/miso/%s/%s' % (bam_dir, self.event_type,
             #                                 sample_id)
 
-            insert_len_commands, insert_len_arguments = self \
-                ._get_psi_insert_len_argument(sample_id, insert_len_file)
+            insert_len_commands, insert_len_arguments = \
+                self._get_psi_insert_len_argument(sample_id, insert_len_file)
 
             # Okay, now we are ready to write to the submitter script
             psi_commands.append('\n\n# --- %s --- #' % sample_id)
