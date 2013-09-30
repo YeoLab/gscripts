6f0a78664d0a1b168439a704989eb21db7b31421
Michael Lovci <michaeltlovci@gmail.com>
2013-09-30 14:49:18 -0700

    added benjamani-hochberg correction for MHT to rnaseq.rpkmZ

--- gscripts/rnaseq/rpkmZ.py
+++ gscripts/rnaseq/rpkmZ.py
@@ -21,10 +21,44 @@ __doc__="""
 >>> pylab.show()
 """
 
+def benjamini_hochberg(pValues, FDR=0.1):
+    """ benjamini-hochberg correction for MHT
+        pValues is a list of pValues
+        FDR is the desired false-discovery rate
+        
+        from: http://udel.edu/~mcdonald/statmultcomp.html
+        "One good technique for controlling the false discovery rate was briefly
+        mentioned by Simes (1986) and developed in detail by Benjamini and Hochberg (1995). 
+        Put the individual P-values in order, from smallest to largest. The smallest 
+        P-value has a rank of i=1, the next has i=2, etc. Then compare each individual 
+        P-value to (i/m)Q, where m is the total number of tests and Q is the chosen false 
+        discovery rate. The largest P-value that has P<(i/m)Q is significant, 
+        and all P-values smaller than it are also significant."
+        
+        """
+    ranks = np.argsort(np.argsort(pValues))
+    
+    nComps = len(pValues) + 0.0
+    pSorter = np.argsort(pValues)
+    pRank = np.argsort(np.argsort(pValues))+1
+    BHcalc = (pRank / nComps) * FDR
+    sigs = np.ndarray(shape=(nComps, ), dtype='bool')
+    issig = True
+    for (p, b, r) in itertools.izip(pValues[pSorter], BHcalc[pSorter], pSorter):
+        if p > b:
+            issig = False
+        sigs[r] = issig
+    return sigs
+
+
+
+
+
+
 
 class Colors(object):
     import numpy as np
-    import matplotlib.colors as clrs    
+    import matplotlib.colors as clrs
     import matplotlib.cm as cmx
     Set1 = cm = pylab.get_cmap('Set1') 
     cNorm  = clrs.Normalize(vmin=min(range(25)), vmax=max(range(25))) #8 colors
@@ -114,9 +148,13 @@ class TwoWayGeneComparison(object):
 
 
 class TwoWayGeneComparison_local(object):
-    def __init__(self, genes1, genes2, pCut = 0.001, local_fraction = 0.1, bonferroni = True):
+    def __init__(self, genes1, genes2, pCut = 0.001, local_fraction = 0.1, bonferroni = True, FDR=None):
         """ Run a two-sample RPKM experiment. Give control sample first, it will go on the x-axis 
             genes1 and genes2 are pandas Series with identical indices
+            pCut - P value cutoff
+            local_fraction - by default the closest 10% of genes are used for local z-score calculation
+            bonferroni - p-values are adjusted for MHT with bonferroni correction
+            BH - benjamini-hochberg FDR filtering
         """
 
 
@@ -170,7 +208,9 @@ class TwoWayGeneComparison_local(object):
             localGenes = self.ranks[self.ranks.between(start, stop)].index
             self.localMean.ix[g] = np.mean(self.log2Ratio.ix[localGenes])
             self.localStd.ix[g] = np.std(self.log2Ratio.ix[localGenes])
-            self.pValues.ix[g] = stats.norm.pdf(self.log2Ratio.ix[g], self.localMean.ix[g], self.localStd.ix[g]) * correction
+            self.pValues.ix[g] = stats.norm.pdf(self.log2Ratio.ix[g],
+                                                self.localMean.ix[g],
+                                                self.localStd.ix[g]) * correction
             self.localZ.ix[g] = (self.log2Ratio.ix[g]- self.localMean.ix[g])/self.localStd.ix[g]
             
         data = pd.DataFrame(index = labels)
@@ -179,6 +219,13 @@ class TwoWayGeneComparison_local(object):
         data["localMean"] = self.localMean
         data["localStd"] = self.localStd
         data["pValue"] = self.pValues
+                
+        if FDR == None:
+            data["isSig"] = self.pValues < pCut
+        else:
+            data["isSig"] = benjamini_hochberg(self.pValues, FDR=FDR)
+    
+                
         data["meanExpression"] = self.average_expression
         data["localZ"] = self.localZ
         data[sampleNames[0]] = genes1
@@ -186,8 +233,11 @@ class TwoWayGeneComparison_local(object):
         
         self.data = data
 
-        for label, (pVal, logratio) in data.get(["pValue", "log2Ratio"]).iterrows():
-            if pVal < pCut:
+    
+       
+            
+        for label, (pVal, logratio, isSig) in data.get(["pValue", "log2Ratio", "isSig"]).iterrows():
+            if (pVal < pCut) and isSig:
                 if logratio > 0:
                     self.upGenes.add(label)
                 elif logratio < 0:
@@ -197,8 +247,8 @@ class TwoWayGeneComparison_local(object):
                     
     def plot(self):
         co = [] #colors container
-        for label, (pVal, logratio) in self.data.get(["pValue", "log2Ratio"]).iterrows():
-            if pVal < self.pCut:
+        for label, (pVal, logratio, isSig) in self.data.get(["pValue", "log2Ratio", "isSig"]).iterrows():
+            if (pVal < self.pCut) and isSig:
                 if logratio > 0:
                     co.append(Colors().redColor)
                 elif logratio < 0:
