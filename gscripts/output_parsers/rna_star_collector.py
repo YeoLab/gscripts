5988a3239d5bc599b4e31bd719364318a92387a8
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-10-02 10:34:43 -0700

    commented out line where log_final_out would print ids

--- gscripts/output_parsers/rna_star_collector.py
+++ gscripts/output_parsers/rna_star_collector.py
@@ -229,7 +229,7 @@ def log_final_out(glob_command, ids_function):
         ids = ids_function
     else:
         ids = [ids_function(filename) for filename in filenames]
-    print 'ids', ids
+    #print 'ids', ids
 
     for filename in filenames:
         s = pd.read_table(filename, header=None, index_col=0, squeeze=True)
