24c1eb2cac94cf77b4cd052ecc889da480c6a4b8
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-10-22 11:48:27 -0700

    fixed quoting error and reformatted via PyCharm

--- gscripts/mapping/map_paired_with_STAR.py
+++ gscripts/mapping/map_paired_with_STAR.py
@@ -4,17 +4,16 @@ import sys
 
 species = sys.argv[1]
 try:
-    name = sys.argv[2]+"_map_to_"+species
+    name = sys.argv[2] + "_map_to_" + species
 except IndexError:
-    name = "map_to_'+species
+    name = "map_to_" + species
 
 cmd_list = []
 
 for file in glob('*R1*fastq'):
-
-	pair = file.replace('R1', 'R2')
-	name = file.replace('_R1', '')
-	cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
+    pair = file.replace('R1', 'R2')
+    name = file.replace('_R1', '')
+    cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
 --runMode alignReads \
 --runThreadN 16 \
 --genomeDir /projects/ps-yeolab/genomes/{}/star/ \
@@ -25,10 +24,9 @@ for file in glob('*R1*fastq'):
 --outFilterMultimapNmax 1'.format(species, file, pair, name))
 
 for file in glob('*R1*norep'):
-
-	pair = file.replace('R1', 'R2')
-	name = file.replace('_R1', '')
-	cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
+    pair = file.replace('R1', 'R2')
+    name = file.replace('_R1', '')
+    cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
 --runMode alignReads \
 --runThreadN 16 \
 --genomeDir /projects/ps-yeolab/genomes/{}/star/ \
@@ -39,10 +37,9 @@ for file in glob('*R1*norep'):
 --outFilterMultimapNmax 1'.format(species, file, pair, name))
 
 for file in glob('*R1*gz'):
-
-	pair = file.replace('R1', 'R2')
-	name = file.replace('_R1', '')
-	cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
+    pair = file.replace('R1', 'R2')
+    name = file.replace('_R1', '')
+    cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
 --runMode alignReads \
 --runThreadN 16 \
 --genomeDir /projects/ps-yeolab/genomes/{}/star/ \
@@ -53,5 +50,7 @@ for file in glob('*R1*gz'):
 --outSAMunmapped Within \
 --outFilterMultimapNmax 1'.format(species, file, pair, name))
 
-sub = Submitter(queue_type='PBS', sh_file=name+'.sh', command_list=cmd_list, job_name=name)
-sub.write_sh(submit=True, nodes=1, ppn=16, walltime='3:00:00', array=True, max_running=15)
+sub = Submitter(queue_type='PBS', sh_file=name + '.sh', command_list=cmd_list,
+                job_name=name)
+sub.write_sh(submit=True, nodes=1, ppn=16, walltime='3:00:00', array=True,
+             max_running=15)
