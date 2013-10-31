66e0c75b93a60afb6feb86afa51294477d8a12e3
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-10-30 21:59:31 -0700

    added remove_spliced_reads.py

---
+++ gscripts/mapping/remove_spliced_reads.py
@@ -0,0 +1,20 @@
+__author__ = 'olga'
+from glob import glob
+from gscripts.qtools._Submitter import Submitter
+import sys
+
+name = "remove_spliced"
+
+try:
+    name = sys.argv[1] + name
+except IndexError:
+    pass
+
+cmd_list = []
+for file in glob('*bam'):
+    cmd_list.append("samtools view -h {0} | awk '$6 ~ /M/ || $1 ~ /~@/' | "
+                    "samtools view -bS - > {0}.unspliced.bam".format(file))
+sub = Submitter(queue_type='PBS', sh_file=name + '.sh', command_list=cmd_list,
+                job_name=name)
+sub.write_sh(submit=True, nodes=1, ppn=16, queue='home', array=True,
+             max_running=10)
