d8004e914106ee71b4e1dfe4a452603761ee9644
Gabriel Pratt <gpratt@ucsd.edu>
2013-11-21 02:56:01 -0800

    updated barcode renamer to work with general length barcodes

--- tests/test_rewrite_barcoded_fastq.py
+++ tests/test_rewrite_barcoded_fastq.py
@@ -6,12 +6,66 @@ Created on Jun 21, 2013
 import unittest
 
 import tests
-from gscripts.clipseq import rewrite_barcoded_fastq
+from gscripts.clipseq.rewrite_barcoded_fastq import reformat_read
 
 class Test(unittest.TestCase):
-
+    def test_reformat_read(self):
         
+        """
+        Tests basic reformatting of read to output known correct result
+        """
+        
+        barcodes = {"GTTG" : "R01"}
 
+        name = "@M01356\n"
+        seq = "CCGGTTGTATTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n"
+        plus = "+\n"
+        quality = "BBBBCBCFFFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n"
+        barcode, randomer, read = reformat_read(name, seq, plus, quality, barcodes)
+        
+        self.assertEqual("GTTG", barcode)
+        self.assertEqual("CCGGTTGTA", randomer)
+        self.assertEqual("@CCGGTTGTA:M01356\nTTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n+\nFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n", read)
+        
+    def test_variable_length_barcodes(self):
+        """
+        
+        Lets see if a length 6 barcode works
+        
+        """
+        
+        barcodes = {"GTTTTG" : "R01"}
+
+        name = "@M01356\n"
+        seq = "CCGGTTTTGTATTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n"
+        plus = "+\n"
+        quality = "FFBBBBCBCFFFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n"
+        barcode, randomer, read = reformat_read(name, seq, plus, quality, barcodes)
+        
+        self.assertEqual("GTTTTG", barcode)
+        self.assertEqual("CCGGTTTTGTA", randomer)
+        self.assertEqual("@CCGGTTTTGTA:M01356\nTTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n+\nFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n", read)
+    
+    def test_multiple_barcodes(self):
+        """
+        
+        Tests if a length 4 and 6 barcode can play together
+        
+        """
+        
+        barcodes = {"GTTG"   : "R00",
+                    "GTTTTG" : "R01"}
+
+        name = "@M01356\n"
+        seq = "CCGGTTTTGTATTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n"
+        plus = "+\n"
+        quality = "FFBBBBCBCFFFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n"
+        barcode, randomer, read = reformat_read(name, seq, plus, quality, barcodes)
+        
+        self.assertEqual("GTTTTG", barcode)
+        self.assertEqual("CCGGTTTTGTA", randomer)
+        self.assertEqual("@CCGGTTTTGTA:M01356\nTTTCATTCTGCCCAGAGCAAAATACATGTGACAAAA\n+\nFFFGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHH\n", read)
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
