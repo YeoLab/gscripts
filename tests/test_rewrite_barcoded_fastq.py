1e3a3ec3f33a59384aaed876b29872757457f4cc
Gabriel Pratt <gpratt@ucsd.edu>
2013-11-21 01:21:36 -0800

    added test shell for unit tests

---
+++ tests/test_rewrite_barcoded_fastq.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 21, 2013
+
+@author: gabrielp
+'''
+import unittest
+
+import tests
+from gscripts.clipseq import rewrite_barcoded_fastq
+
+class Test(unittest.TestCase):
+
+        
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
