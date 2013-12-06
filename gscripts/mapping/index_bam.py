7e9a687086941fb19ae883a01d3fe75609d7334f
Olga Botvinnik <olga.botvinnik@gmail.com>
2013-12-06 09:05:48 -0800

    reformatting index_bam

--- gscripts/mapping/index_bam.py
+++ gscripts/mapping/index_bam.py
@@ -8,9 +8,12 @@ except IndexError:
     name = "index_bam"
 
 cmd_list = []
-for file in glob('*sorted.bam'):
 
-	cmd_list.append('samtools index {}'.format(file))
-sub = Submitter(queue_type='PBS', sh_file=name+'.sh', command_list=cmd_list, job_name=name)
-sub.write_sh(submit=True, nodes=1, ppn=1, queue='home', walltime='1:00:00', array=True, max_running=20)
+for file in glob('*sorted.bam'):
+    cmd_list.append('samtools index {}'.format(file))
+
+sub = Submitter(queue_type='PBS', sh_file=name + '.sh', command_list=cmd_list,
+                job_name=name)
+sub.write_sh(submit=True, nodes=1, ppn=1, queue='home', walltime='1:00:00',
+             array=True, max_running=20)
 
