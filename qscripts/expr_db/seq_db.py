8e985b1f4d0b4c52f3169aa216c30e05f9671528
ppliu <uil.kcirtap@gmail.com>
2014-01-06 14:39:27 -0800

    added beginning db func for seq database

--- qscripts/expr_db/seq_db.py
+++ qscripts/expr_db/seq_db.py
@@ -1,22 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from collections import defaultdict
+import itertools
+import string
+import random
 
 engine = create_engine('mysql+pymysql://ppliu:some_pass@sauron.ucsd.edu/test')
 Base = declarative_base()
 
 class SeqExpr(Base):
+
     __tablename__ = 'seqexpr'
     id = Column(Integer, primary_key = True)
     sample_name = Column(String(100))
+    expr_name = Column(String(100))
     file_location = Column(String(200))
     pair_location = Column(String(200))
-    species = Column(String(5))
-    output_directory = ''
-    compressed = False
+    species = Column(String(50))
     type = Column(String(50))
     collab = Column(String(50))
-    
+    output_directory = ''
+    compressed = False
 
     __mapper_args__ = {
         'polymorphic_identity':'seqexpr',
@@ -25,11 +30,129 @@ class SeqExpr(Base):
         
 
 class RNASeq(SeqExpr):
-    __tablename__ = 'rnaseq'    
+    
+    # sqlalchemy specific
+    __tablename__ = 'rnaseq'  
     id = Column(Integer, ForeignKey('seqexpr.id'), primary_key=True)
     strand = Column(String(50))
-    
+    adapter = Column(String(50))   
+
     __mapper_args__ = {
         'polymorphic_identity':'rnaseq'
     }
 
+    def get_params(self):
+        return (self.__tablename__, self.species, self.strand, self.adapter)
+
+
+class CLIPSeq(SeqExpr):
+    
+    __tablename__ = 'clipseq'
+    id = Column(Integer, ForeignKey('seqexpr.id'), primary_key=True)
+
+    def get_params(self):
+        return (self.__tablename__, self.species)
+
+def q_setup(expr_list, working_dir='~/scratch/', session_name=None, queue='home', group='yeo'):
+
+    star_genome = {
+        'hg19': '/projects/ps-yeolab/genomes/hg19/star/',
+        'mm9': '/projects/ps-yeolab/genomes/mm9/star/',
+        'ce10': '/projects/ps-yeolab/genomes/ce10/star/',
+        'dm3': '/projects/ps-yeolab/genomes/dm3/star/'
+    }
+    
+    tags_annotation = {
+        'hg19': '/projects/ps-yeolab/genomes/hg19/gencode_v17/gencode.v17.annotation.exons.bed',
+        'mm9': '/projects/ps-yeolab/genomes/mm9/Mus_musculus.NCBIM37.64.fixed.exons.bed'
+    }
+    
+    chr_sizes = {
+        'hg19': '/projects/ps-yeolab/genomes/hg19/hg19.chrom.sizes',
+        'mm9': '/projects/ps-yeolab/genomes/mm9/mm9.chrom.sizes'
+    }
+
+    scala_script = {
+        'rnaseq': '~/gscripts/qscripts/analyze_rna_seq.scala',
+        'clipseq': '~/gscripts/qscripts/analyze_clip_seq.scala'
+    }
+
+
+    params = [[expr.get_params(), expr] for expr in expr_list]
+    for k, v in itertools.groupby(params, lambda x:"".join(x[0])):
+
+
+        if session_name == None:
+
+            sh_name = ''
+            filelist_name = ''
+            filelist_name = random.choice(string.letters)
+            filelist_name += random.choice(string.letters)
+            filelist_name += random.choice(string.letters)
+            filelist_name += random.choice(string.letters)
+            filelist_name += random.choice(string.letters)
+            sh_name = filelist_name + '.sh'
+            filelist_name += '.txt'
+       
+        else:
+            sh_name = session_name + '.sh'
+            filelist_name = session_name + '.txt'
+ 
+        filelist_name = working_dir + '/' +filelist_name
+        sh_name = working_dir + '/' + sh_name
+        print filelist_name
+       
+        fl = open(filelist_name, 'w') 
+        for item in list(v):
+            print item[1].file_location
+            fl.write(item[1].file_location)
+            if item[1].pair_location:
+                fl.write('\t')
+                fl.write(item[1].pair_location)
+            fl.write('\n')
+            
+        fl.close()
+        
+        if expr.__tablename__ == 'rnaseq':
+            command = 'java -Xms512m -Xmx512m -jar ~/dash/gatk/dist/Queue.jar'
+            command += ' -S '+ scala_script[expr.__tablename__]
+            command += ' --input ' + filelist_name
+            command += ' --species ' + expr.species
+            command += ' --chr_sizes ' + chr_sizes[expr.species]
+            command += ' --tags_annotation ' + tags_annotation[expr.species]
+            command += ' --star_genome_location ' + star_genome[expr.species]
+            command += ' --adapter ' + expr.adapter
+            command += ' -jobQueue ' + queue
+            command += ' -jobNative ' + '\"group_list={}-group\"'.format(group)
+            command += ' --compressed '
+            command += ' -run '
+            command += ' -qsub '
+
+        elif expr.__tablename__ == 'clipseq'
+            
+            command = 'java -Xms512m -Xmx512m -jar ~/dash/gatk/dist/Queue.jar'
+            command += ' -S '+ scala_script[expr.__tablename__]
+            command += ' --input ' + filelist_name
+            command += ' --species ' + expr.species
+            command += ' --chr_sizes ' + chr_sizes[expr.species]
+            command += ' --tags_annotation ' + tags_annotation[expr.species]
+            command += ' --star_genome_location ' + star_genome[expr.species]
+            command += ' --adapter ' + 'AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT'
+            command += ' --adapter ' + 'CAAGCAGAAGACGGCATACGAGATCGGTCTCGGCATTCCTGCTGAACCGCTCTTCCGATCT'
+            command += ' --adapter ' + 'ATCTCGTATGCCGTCTTCTGCTTG'
+            command += ' --adapter ' + 'CGACAGGTTCAGAGTTCTACAGTCCGACGATC'
+            command += ' --adapter ' + GATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
+            command += ' -jobQueue ' + queue
+            command += ' -jobNative ' + '\"group_list={}-group\"'.format(group)
+            command += ' --compressed '
+            command += ' -run '
+            command += ' -qsub '
+
+        sh = open(sh_name, 'w')
+        print k
+        print command
+        sh.write(command)
+        sh.close()
+
+
+   
