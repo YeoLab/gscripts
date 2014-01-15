e3289e881cf570f19330a0924b8c2fc6c6fd8316
Gabriel Pratt <gpratt@ucsd.edu>
2014-01-14 17:09:38 -0800

    added rnaseqc and riboseq coverage plots to respective pipelines

---
+++ gscripts/rnaseq/make_rnaseqc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#makes and outputs to stdout a formatted file to run rnaseq-qc on
+import sys
+import os
+
+for sample in sys.argv[1:]:
+    print "\t".join([os.path.basename(sample).split(".")[0], os.path.abspath(sample), "foo"])
