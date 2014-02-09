6fcfebb46a61bd34fbba4c8cc3788feaef880b85
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-02-09 12:46:09 -0800

    index_bam and sort_bam properly handle >500 files as multiple array jobs

--- gscripts/mapping/sort_bam.py
+++ gscripts/mapping/sort_bam.py
@@ -7,13 +7,30 @@ try:
 except IndexError:
     name = "sort_bam"
 
-cmd_list = []
+command_list = []
 for file in glob('*bam'):
-    cmd_list.append('samtools sort -m 50000000000 {0} {0}.sorted'.format(
+    command_list.append('samtools sort -m 50000000000 {0} {0}.sorted'.format(
         file))
 
-sub = Submitter(queue_type='PBS', sh_file=name + '.sh', command_list=cmd_list,
-                job_name=name)
-sub.write_sh(submit=True, nodes=1, ppn=16, queue='home', array=True,
-             max_running=10)
+
+def submit_and_write(name, command_list):
+    sub = Submitter(queue_type='PBS', sh_file=name + '.sh',
+                    command_list=command_list,
+                    job_name=name)
+
+    sub.write_sh(submit=True, nodes=1, ppn=16, queue='home', array=True,
+                 max_running=10)
+
+
+# max number of jobs in an array on TSCC is 500
+if len(command_list) > 500:
+    command_list_list = [command_list[i:(i + 500)] for i in xrange(0,
+                                                                   len(
+                                                                       command_list),
+                                                                   500)]
+    for i, commands in enumerate(command_list_list):
+        submit_and_write('{}{}'.format(name, i + 1), commands)
+else:
+    submit_and_write(name, command_list)
+
 
