7e3d5ab12b16ffdd1bfd2f704f35821ced977afe
Boyko Kakaradov <bkakarad@tscc-login2.sdsc.edu>
2014-03-14 09:47:22 -0700

    commented out run_kasey.py in setup.py because it doesn't exist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 scripts = [
     'annotations/gencode_annotate_utrs_5p_3p.pl',
-    'clipseq/run_kasey.py',
+#    'clipseq/run_kasey.py',
     'clipseq/kmer_extractor.py',
     'clipseq/perform_idr.py',
     'clipseq/run_piranha.py',
