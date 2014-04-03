dbfbdbb994b0fbae97cb679ac74d14018d9c40a5
Michael Lovci <michaeltlovci@gmail.com>
2014-04-03 12:12:01 -0700

    updates for compatibility with seaborn 0.3 API changes

--- gscripts/general/dataviz.py
+++ gscripts/general/dataviz.py
@@ -20,6 +20,33 @@ from scipy.stats import gaussian_kde
 from sklearn import decomposition as dc
 import statsmodels.api as sm
 import seaborn
+
+seaborn.set_style({'axes.axisbelow': True,
+                 'axes.edgecolor': '.15',
+                 'axes.facecolor': 'white',
+                 'axes.grid': False,
+                 'axes.labelcolor': '.15',
+                 'axes.linewidth': 1.25,
+                 'font.family': 'Helvetica',
+                 'grid.color': '.8',
+                 'grid.linestyle': '-',
+                 'image.cmap': 'Greys',
+                 'legend.frameon': False,
+                 'legend.numpoints': 1,
+                 'legend.scatterpoints': 1,
+                 'lines.solid_capstyle': 'round',
+                 'text.color': '.15',
+                 'xtick.color': '.15',
+                 'xtick.direction': 'out',
+                 'xtick.major.size': 0,
+                 'xtick.minor.size': 0,
+                 'ytick.color': '.15',
+                 'ytick.direction': 'out',
+                 'ytick.major.size': 0,
+                 'ytick.minor.size': 0})
+
+seaborn.set_color_palette('deep')
+
 import pylab
 
 
