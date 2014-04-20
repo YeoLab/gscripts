cd7c6e4093d9033c9af7c2ef3a4290179701f036
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-20 12:07:50 -0700

    ad absolute paths

--- gscripts/mapping/map_paired_with_STAR.py
+++ gscripts/mapping/map_paired_with_STAR.py
@@ -51,8 +51,8 @@ for read1 in glob('*R1*gz'):
 --genomeDir /projects/ps-yeolab/genomes/{0}/star_sjdb/ \
 --genomeLoad LoadAndRemove \
 --readFilesCommand zcat \
---readFilesIn {2},{3} \
---outFileNamePrefix {4}. \
+--readFilesIn $PBS_O_WORKDIR/{2},$PBS_O_WORKDIR/{3} \
+--outFileNamePrefix $PBS_O_WORKDIR/aligned/{4}. \
 --outSAMunmapped Within \
 --outReadsUnmapped Fastx \
 --outFilterMismatchNmax 5 \
