58e0131aa49c248195bab08e77ab723eaee8b7fa
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-20 11:32:58 -0700

    comment out other map_paired_with_star so only use the last one

--- gscripts/mapping/map_paired_with_STAR.py
+++ gscripts/mapping/map_paired_with_STAR.py
@@ -12,31 +12,31 @@ except IndexError:
 
 cmd_list = []
 
-for file in glob('*R1*fastq'):
-    pair = file.replace('R1', 'R2')
-    name = file.replace('_R1', '')
-    cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
---runMode alignReads \
---runThreadN 16 \
---genomeDir /projects/ps-yeolab/genomes/{}/star/ \
---genomeLoad LoadAndRemove \
---readFilesIn {} {} \
---outFileNamePrefix {}. \
---outSAMunmapped Within \
---outFilterMultimapNmax 1'.format(species, file, pair, name))
-
-for file in glob('*R1*norep'):
-    pair = file.replace('R1', 'R2')
-    name = file.replace('_R1', '')
-    cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
---runMode alignReads \
---runThreadN 16 \
---genomeDir /projects/ps-yeolab/genomes/{}/star/ \
---genomeLoad LoadAndRemove \
---readFilesIn {} {} \
---outFileNamePrefix {}. \
---outSAMunmapped Within \
---outFilterMultimapNmax 1'.format(species, file, pair, name))
+# for file in glob('*R1*fastq'):
+#     pair = file.replace('R1', 'R2')
+#     name = file.replace('_R1', '')
+#     cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
+# --runMode alignReads \
+# --runThreadN 16 \
+# --genomeDir /projects/ps-yeolab/genomes/{}/star/ \
+# --genomeLoad LoadAndRemove \
+# --readFilesIn {} {} \
+# --outFileNamePrefix {}. \
+# --outSAMunmapped Within \
+# --outFilterMultimapNmax 1'.format(species, file, pair, name))
+#
+# for file in glob('*R1*norep'):
+#     pair = file.replace('R1', 'R2')
+#     name = file.replace('_R1', '')
+#     cmd_list.append('/home/yeo-lab/software/STAR_2.3.0e/STAR \
+# --runMode alignReads \
+# --runThreadN 16 \
+# --genomeDir /projects/ps-yeolab/genomes/{}/star/ \
+# --genomeLoad LoadAndRemove \
+# --readFilesIn {} {} \
+# --outFileNamePrefix {}. \
+# --outSAMunmapped Within \
+# --outFilterMultimapNmax 1'.format(species, file, pair, name))
 
 for read1 in glob('*R1*gz'):
     read2 = read1.replace('R1', 'R2')
@@ -60,4 +60,5 @@ sub = Submitter(queue_type='PBS', sh_file=jobname + '.sh',
                 command_list=cmd_list,
                 job_name=jobname)
 sub.write_sh(submit=True, nodes=1, ppn=8, walltime='0:30:00', use_array=True,
+             array=True,
              max_running=20)
