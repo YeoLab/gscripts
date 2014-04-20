cc787d2fb2a5bbfc83806d5de66b0f5f2d144928
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-20 11:38:23 -0700

    add absolute path for in and out read files

--- gscripts/mapping/map_paired_with_STAR.py
+++ gscripts/mapping/map_paired_with_STAR.py
@@ -3,6 +3,7 @@
 from glob import glob
 from gscripts.qtools._Submitter import Submitter
 import sys
+import os
 
 species = sys.argv[1]
 try:
@@ -38,6 +39,8 @@ cmd_list = []
 # --outSAMunmapped Within \
 # --outFilterMultimapNmax 1'.format(species, file, pair, name))
 
+pwd = os.path.abspath(os.path.curdir)
+
 for read1 in glob('*R1*gz'):
     read2 = read1.replace('R1', 'R2')
     name = '_'.join(read1.split('_'[:2]))
@@ -47,14 +50,14 @@ for read1 in glob('*R1*gz'):
 --genomeDir /projects/ps-yeolab/genomes/{0}/star_sjdb/ \
 --genomeLoad LoadAndRemove \
 --readFilesCommand zcat \
---readFilesIn {1} {2} \
---outFileNamePrefix {3}. \
+--readFilesIn {1}/{2} {1}/{3} \
+--outFileNamePrefix {1}/{4}. \
 --outSAMunmapped Within \
 --outReadsUnmapped Fastx \
 --outFilterMismatchNmax 5 \
 --clip5pNbases 10 \
 --clip3pNbases 10 \
---outFilterMultimapNmax 5'.format(species, read1, read2, name))
+--outFilterMultimapNmax 5'.format(species, pwd, read1, read2, name))
 
 sub = Submitter(queue_type='PBS', sh_file=jobname + '.sh',
                 command_list=cmd_list,
