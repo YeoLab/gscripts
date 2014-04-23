8756dc0854b09ac47200a0294f52408ba669c887
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-23 15:57:05 -0700

    add command line template and started rewrite of map_paired_with_star.py to have better command line arguments

--- gscripts/mapping/map_paired_with_STAR.py
+++ gscripts/mapping/map_paired_with_STAR.py
@@ -84,3 +84,87 @@ sub = Submitter(queue_type='PBS', sh_file=jobname + '.sh',
 sub.write_sh(submit=True, nodes=1, ppn=8, walltime='0:20:00', use_array=True,
              array=True,
              max_running=20)
+
+import argparse
+
+
+class CommandLine(object):
+    def __init__(self, inOpts=None):
+        self.parser = parser = argparse.ArgumentParser(
+            description='Run the RNA-STAR aligner on fastq or fastq.gz files. '
+                        'Submits a job to TSCC (must be logged on to TSCC)'
+                        'This will ONLY map. If you want to do all the stuff '
+                        'you normally do for RNA-seq like quantify isoforms '
+                        'and count RPKM/FPKMs and test for splicing, talk to '
+                        'Gabe about')
+        parser.add_argument('species', required=True, type=str,
+                            action='store',
+                            help='Species to map to')
+        parser.add_argument('jobname', required=False, action='store',
+                            type=str, help="Default job name is 'map_to_'{"
+                                           "species}', and this will prepend "
+                                           "the job name submitted to TSCC "
+                                           "and the .sh file created")
+        parser.add_argument('-o', '--out-dir', default='./',
+                            action='store', type=str,
+                            help='Where to output the aligned files')
+        parser.add_argument('--do-not-submit', action='store_true',
+                            help='')
+        parser.add_argument('-s', '--with-sjdb', required=False,
+                            action='store_true',
+                            help='Whether or not to use the genome '
+                                 'created with the splice junction '
+                                 'database')
+        parser.add_argument('-w', '--walltime', default='0:20:00',
+                            type=str, action='store')
+
+        if inOpts is None:
+            self.args = vars(self.parser.parse_args())
+        else:
+            self.args = vars(self.parser.parse_args(inOpts))
+
+    def do_usage_and_die(self, str):
+        '''
+        If a critical error is encountered, where it is suspected that the
+        program is not being called with consistent parameters or data, this
+        method will write out an error string (str), then terminate execution
+        of the program.
+        '''
+        import sys
+
+        print >> sys.stderr, str
+        self.parser.print_usage()
+        return 2
+
+
+# Class: Usage
+class Usage(Exception):
+    '''
+    Used to signal a Usage error, evoking a usage statement and eventual
+    exit when raised
+    '''
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+if __name__ == '__main__':
+    cl = CommandLine()
+
+    sjdb_arguments = ['sjdbGTFfile', 'sjdbFileChrStartEnd']
+
+    sjdb = ''.join('--{} {}'.format(k, cl.args[k]) for k in sjdb_arguments
+                   if cl.args[k])
+    commands = []
+    commands.append('STAR --runMode genomeGenerate --genomeDir {0} '
+                    '--genomeFastaFiles {1} --runThreadN 16 {2}'.format(
+        cl.args['genomeDir'], cl.args['genomeFastaFiles'], sjdb
+    ))
+
+    name = cl.args['name']
+
+    sub = Submitter(queue_type='PBS', sh_file=name + '.sh',
+                    command_list=commands,
+                    job_name=name)
+    sub.write_sh(submit=True, nodes=1, ppn=16, queue='home',
+                 walltime='4:00:00')
