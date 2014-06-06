cb0259ddff9e4526aa6c16005b9824bcfa38875d
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-06 16:58:30 -0700

    add matplotlib helpers like for making your legend outside the figure

--- gscripts/ipython_imports.py
+++ gscripts/ipython_imports.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 import brewer2mpl
 import itertools
 import seaborn as sns
+
+sns.set(style='ticks', context='talk')
+
+import matplotlib as mpl
+# Set the svg output text as actual text and not outlines of paths
+# (easier to edit)
+mpl.rcParams['svg.fonttype'] = 'none'
+
 import collections
 import itertools
 
@@ -18,3 +26,20 @@ yellow = set1[5]
 brown = set1[6]
 pink = set1[7]
 grey = set1[8]
+
+
+# To put the legend outside the plot, on the right side, add these keyword
+# arguments ("kwargs") to your legend making, e.g.:
+# legend = ax.legend(**OUTSIDE_LEGEND_KWARGS)
+OUTSIDE_LEGEND_KWS = dict(bbox_to_anchor=(1, 0.5), loc='center left')
+
+# As a placeholder. Should be the output from
+# "ax.legend(**OUTSIDE_LEGEND_KWARGS)"
+legend = None
+
+# Also need to save the figure in a special way so that the legend doesn't
+# get cut off. Make sure to save the "legend" object that "ax.legend" returns
+# and then save that as "legend", then add these keyword arguments to your
+# savefig call:
+OUTSIDE_LEGEND_SAVEFIG_KWS = dict(bbox_extra_artists=(legend,),
+                                  bbox_inches='tight')
