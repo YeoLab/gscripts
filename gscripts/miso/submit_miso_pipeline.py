1e4af2e11691ef64a91c118758e41f5388b4fa9a
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-06-30 12:52:40 -0700

    Greatly simplified Miso submitter code

--- gscripts/miso/submit_miso_pipeline.py
+++ gscripts/miso/submit_miso_pipeline.py
@@ -4,6 +4,7 @@
 # Parse command line arguments
 import argparse
 import os
+import pandas as pd
 import sys
 
 # Submit jobs to the cluster
@@ -53,22 +54,25 @@ class CommandLine(object):
     def __init__(self, inOpts=None):
         self.parser = argparse.ArgumentParser(
             description='''Write a script to perform MISO analysis
-            on individual samples. This script must be submitted to
+            on individual samples. By default, this is submitted to TSCC,
+            unless "--do-not-submit" is specified.
             ''',
             add_help=True, prefix_chars='-')
-
-        self.parser.add_argument('--sample-id', type=str,
-                                 action='store',
-                                 help='sample ID. required if using --bam',
-                                 required=False)
         samples = self.parser.add_mutually_exclusive_group(required=True)
         samples.add_argument('--bam', type=str,
                              action='store', default='',
                              help='A single BAM file')
         samples.add_argument('--sample-info', type=str, action='store',
                              help='A tab-delimited file with Bam files as '
-                                  'column 1 and ')
+                                  'column 1 and the sample Ids as column 2 ('
+                                  'no header). In conjunction with '
+                                  '"--actually-submit", this will submit an '
+                                  'array job to be a nice cluster user')
 
+        self.parser.add_argument('--sample-id', type=str,
+                                 action='store',
+                                 help='sample ID. required if using --bam',
+                                 required=False)
         self.parser.add_argument('--debug', action='store_true',
                                  default=False,
                                  help="Don't make any files, just print "
@@ -78,40 +82,6 @@ class CommandLine(object):
         self.parser.add_argument('--output-sh', type=str, required=True,
                                  action='store',
                                  help="The name of the .sh script created for one-touch action")
-        self.parser.add_argument('--summary-output-dir-base', type=str,
-                                 action='store', default='',
-                                 help='The base directory for which to place '
-                                      'the MISO summary outputs. By '
-                                      'default, MISO outputs are of '
-                                      'the format: (base_dir)/miso/('
-                                      'event_type)/(sample_id). The default '
-                                      'base dir is the directory of the .bam '
-                                      'file, e.g. if the bam you provide is '
-                                      'in ~/scratch/single_cell and your '
-                                      'event type is "SE", then miso outputs '
-                                      'for sample id "A1_02"'
-                                      ' will be in the folder'
-                                      '~/scratch/single_cell/miso/SE/A1_02/. '
-                                      'However, the intermediate output would'
-                                      ' still be in (bam_dir)/miso/('
-                                      'event_type)/(sample_id) because MISO '
-                                      'outputs a TON of intermediate files '
-                                      'that nobody wants to deal with.'
-                                      'Otherwise, if you provide a folder '
-                                      'such as '
-                                      '~/projects/single_cell/analysis, '
-                                      'then the MISO summary output for the '
-                                      'same '
-                                      'sample would be in: '
-                                      '~/projects/single_cell/analysis/miso/SE/A1_02')
-        self.parser.add_argument('--sample-id-suffix', type=str,
-                                 action='store', default='',
-                                 help='Extra identification to add to these '
-                                      'samples, e.g. if you are running with '
-                                      'a settings file that specifies a '
-                                      'minimum of 10 reads instead of 20, '
-                                      'you could say "_min_event_reads10" as '
-                                      'a suffix')
         self.parser.add_argument('--extra-miso-arguments', type=str,
                                  action='store',
                                  default='',
@@ -131,6 +101,10 @@ class CommandLine(object):
                                       'quote and the first dash of the miso '
                                       'argument. For some reason this helps..'
                                       '..')
+        self.parser.add_argument('--do-not-submit', type=bool,
+                                 action='store', default=False,
+                                 help='Whether or not to actually submit the '
+                                      'final file.')
 
         if inOpts is None:
             self.args = vars(self.parser.parse_args())
@@ -163,77 +137,79 @@ class Usage(Exception):
 
 
 class MisoPipeline(object):
-    def __init__(self, bam, sample_id, output_sh,
+    def __init__(self, bam, sample_info_file,
+                 sample_id, output_sh,
                  genome,
-                 read_type='single_end',
-                 debug=False, num_processes=16,
-                 submit_sh_suffix='',
-                 sample_id_suffix='',
-                 extra_miso_arguments=''):
+
+                 debug=False,
+                 extra_miso_arguments='',
+                 submit=False):
         """
-        Given a CommandLine object 'cl', save the arguments as
-        attributes of this class. And initialize the cluster job IDs as None,
-         so we can check if they're there or not in the future.
+        Parameters
+        ----------
+
+
+        Returns
+        -------
+
+
+        Raises
+        ------
         """
-        self.read_type = read_type
-        #self.event_type = cl.args['event_type'].upper()
-        # self.sample_info_file = cl.args['sample_info_file']
+        self.sample_info_file = sample_info_file
 
-        self.debug = debug
-        self.num_processes = num_processes
-
-        self.bam = bam
-        self.sample_id = sample_id
-        self.sample_ids = [sample_id]
-        self.bams = [bam]
-
-        self.output_sh = output_sh
-
-        self.extra_miso_arguments = extra_miso_arguments
-
-        if submit_sh_suffix != '':
-            self.submit_sh_suffix = '_' + submit_sh_suffix.lstrip('_')
+        if self.sample_info_file is not None:
+            sample_info = pd.read_table(self.sample_info_file)
+            self.bams = sample_info[0]
+            self.sample_ids = sample_info[1]
+            self.sh_files = ['{}.miso.sh'.format(bam) for bam in self.bams]
+            self.multiple_samples = True
         else:
-            self.submit_sh_suffix = ''
-        if sample_id_suffix != '':
-            self.sample_id_suffix = '_' + sample_id_suffix.lstrip('_')
-            self.sample_ids = [sample_id + self.sample_id_suffix
-                               for sample_id in self.sample_ids]
-        else:
-            self.sample_id_suffix = ''
-
-        #self.job_name_prefix = 'miso%s_%s' % (self.submit_sh_suffix,
-        #                                      self.event_type)
+            self.sample_ids = [sample_id]
+            self.bams = [bam]
+            self.sh_files = [output_sh]
+            self.multiple_samples = False
 
         self.genome = genome
+        self.debug = debug
+        self.extra_miso_arguments = extra_miso_arguments
+        self.submit = submit
 
+        all_samples_commands = []
 
-    def run_all_single_sample(self):
+        for bam, sample_id, sh_file in zip(self.bams, self.sample_ids,
+                                           self.sh_files):
+            self._write_single_sample(bam, sample_id, sh_file)
 
-        bam = self.bam
-        sample_id = self.sample_id
+            sh_command = 'bash {}'.format(sh_file)
+            if self.submit and not self.multiple_samples:
+                commands = [sh_command]
+                sub = Submitter(commands, job_name='miso',
+                                sh_filename='{}.qsub.sh'.format(sh_file),
+                                ppn=16, walltime='1:00:00')
+                sub.job(submit=self.submit)
 
-        bam_dir = os.path.dirname(os.path.abspath(bam))
+            if self.multiple_samples:
+                all_samples_commands.append(sh_command)
 
+        if self.multiple_samples:
+            sub = Submitter(all_samples_commands, job_name='miso',
+                            sh_filename=output_sh,
+                            array=True, ppn=16, walltime='1:00:00')
+            sub.job(submit=self.submit)
+
+    def _write_single_sample(self, bam, sample_id, sh_file):
         commands = []
         commands.append('#!/bin/bash')
         commands.append('# Finding all MISO splicing scores for sample: {}. '
                         'Yay!\n'
                         .format(sample_id))
 
-        insert_len_arguments = ''
-
         event_types = ['SE', 'MXE', 'AFE', 'ALE', 'A3SS', 'A5SS',
                        'RI', 'TANDEMUTR']
 
         # Get the read length. Gonna keep this as bash because samtools
-        # and less are very fast
-        #commands.append(
-        #    '\n# Assuming that the first read of the bam file is '
-        #    'representative, such that all the reads in the '
-        #    '\n# file are exactly the same length, we can take the first '
-        #    'read from the bam file and measure its length, '
-        #    '\n# and use that for our algorithm')
+        # and head are very fast
         commands.append(
             "READ_LEN=$(samtools view %s | head -n 1 | cut -f 10 | awk '{ "
             "print length }')" % (bam))
@@ -249,17 +225,15 @@ class MisoPipeline(object):
                             ' all {} events'.format(event_type))
             commands.append('mkdir -p {}'.format(out_dir))
             commands.append('python /projects/ps-yeolab/software/bin/miso \
- --run /projects/ps-yeolab/genomes/{0}/miso/{1}_index \
- {2} --output-dir {3} \
- --read-len $READ_LEN \
-{4} \
- --settings-filename /projects/ps-yeolab/genomes/hg19/miso_annotations'
+         --run /projects/ps-yeolab/genomes/{0}/miso/{1}_index \
+         {2} --output-dir {3} \
+         --read-len $READ_LEN \
+         --settings-filename /projects/ps-yeolab/genomes/hg19/miso_annotations'
                             '/miso_settings_min_event_reads10.txt \
- -p {5} \
+ -p 16 \
  > {6} \
  2> {7}'.format(self.genome, event_type, bam, out_dir,
-                insert_len_arguments, self.num_processes, psi_out,
-                psi_err))
+                psi_out, psi_err))
 
             commands.append("\n# Check that the psi calculation jobs didn't "
                             "fail.\n#'-z' "
@@ -287,12 +261,12 @@ fi\n".format(out_dir, event_type))
             commands.append("# '-s' returns true if file size is nonzero, "
                             "and the error file should be empty.")
             commands.append("""if [ -s {0}/summary.err ] ; then
-    #rm -rf {0}\n
-    echo 'MISO psi failed on event type: {1}'
-    exit 1
-fi
-""".format(out_dir, event_type))
-        sh_file = self.output_sh
+            #rm -rf {0}\n
+            echo 'MISO psi failed on event type: {1}'
+            exit 1
+        fi
+        """.format(out_dir, event_type))
+
         with open(sh_file, 'w') as f:
             f.write('\n'.join(commands))
         sys.stdout.write('Wrote miso script for sample "{}": {}\n'.format(
@@ -312,32 +286,16 @@ def main():
     '''
     cl = CommandLine()
     try:
-        miso_pipeline = MisoPipeline(cl.args['bam'], cl.args['sample_id'],
-                                     cl.args['output_sh'],
-                                     cl.args['genome'],
-                                     debug=cl.args['debug'],
-                                     num_processes=cl.args['num_processes'],
-                                     submit_sh_suffix=cl.args[
-                                         'submit_sh_suffix'],
-                                     sample_id_suffix=cl.args[
-                                         'sample_id_suffix'],
-                                     extra_miso_arguments=cl.args[
-                                         'extra_miso_arguments'])
+        submit = not cl.args['do_not_submit']
 
-        # Read the arguments to see which piece of the MISO pipeline to run
-        #if cl.args['run_all']:
-        #    if cl.args['single_sample']:
-        miso_pipeline.run_all_single_sample()
-        #else:
-        #    miso_pipeline.run_all()
-        #elif cl.args['insert_len_only']:
-        #    miso_pipeline.insert_len()
-        #elif cl.args['psi_only']:
-        #    miso_pipeline.psi()
-        #elif cl.args['summary_only']:
-        #    miso_pipeline.summary()
-        #elif cl.args['psi_and_summary']:
-        #    miso_pipeline.psi_and_summary()
+        MisoPipeline(cl.args['bam'],
+                     cl.args['sample_info'],
+                     cl.args['sample_id'],
+                     cl.args['output_sh'],
+                     cl.args['genome'],
+                     debug=cl.args['debug'],
+                     extra_miso_arguments=cl.args['extra_miso_arguments'],
+                     submit=submit)
 
     # If not all the correct arguments are given, break the program and
     # show the usage information
