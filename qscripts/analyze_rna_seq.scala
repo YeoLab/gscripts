6014eccc509b4948b9d66dfc8a499a2227d88908
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-02-08 16:15:38 -0800

    fix trim galore output

--- qscripts/analyze_rna_seq.scala
+++ qscripts/analyze_rna_seq.scala
@@ -217,10 +217,8 @@ class AnalyzeRNASeq extends QScript {
 
     // run if stringent
 
-    val outDir = swapExt(fastqFile, ".fastq", "_trim_galore")
-
-    val trimmedFastq = outDir + "/" + swapExt(fastqFile, ".fastq", "_trimmed.fastq")
-    val trimmedFastqPair = outDir + "/" + swapExt(fastqPair, ".fastq", "_trimmed.fastq")
+    val trimmedFastq = swapExt(fastqFile, ".fastq.gz", "_trimmed.fastq")
+    val trimmedFastqPair = swapExt(fastqPair, ".fastq.gz", "_trimmed.fastq")
 
     val filteredFastq = swapExt(fastqFile, ".fastq", ".polyATrim.adapterTrim.rmRep.fastq").replace("R1", "R%")
     val filteredFastqPair = swapExt(fastqFile, ".fastq", ".polyATrim.adapterTrim.rmRep.fastq").replace("R2", "R%")
