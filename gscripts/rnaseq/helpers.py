85208a951f8aacba71145014c712f11efbdbf84d
gpratt <gpratt@gmail.com>
2015-06-17 14:05:57 -0700

    updated count_to_rpkm to detect if a string is present, assume its a file and convert that to RPKMs as well

--- gscripts/rnaseq/helpers.py
+++ gscripts/rnaseq/helpers.py
@@ -1,9 +1,11 @@
 __author__ = 'gpratt'
-
+import pandas as pd
 def counts_to_rpkm(featureCountsTable):
     """
-    Given a dataframe of counts converts that dataframe into RPKMs
+    Given a dataframe or a text file from featureCounts and converts that thing into a dataframe of RPKMs
     """
+    if isinstance(featureCountsTable, str):
+        featureCountsTable = pd.read_table(featureCountsTable, skiprows=1, index_col=0)
 
     counts = featureCountsTable.ix[:, 5:]
     lengths = featureCountsTable['Length']
