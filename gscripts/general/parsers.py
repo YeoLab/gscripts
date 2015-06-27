33c91c6c8d9374974076d66262f179a38078ce2c
gpratt <gpratt@gmail.com>
2015-06-27 11:23:00 -0700

    fixed parse_cutadapt to work with cutadapt 1.8 and 1.9 gracefully in my parsers

--- gscripts/general/parsers.py
+++ gscripts/general/parsers.py
@@ -208,7 +208,25 @@ def parse_rm_duped_metrics_file(rmDup_file):
                     "removed_count": None,
                     "Usable Reads": None}
 
+
+def get_cutadapt_version(report):
+    with open(report) as file_handle:
+        version = file_handle.next()
+        version = version.split()[-4]
+
+    return int(version.split(".")[1])
+
+
 def parse_cutadapt_file(report):
+
+    old_cutadapt = get_cutadapt_version(report) <= 8
+    print old_cutadapt
+    if old_cutadapt:
+        return parse_old_cutadapt_file(report)
+    else:
+        return parse_new_cutadapt_file(report)
+
+def parse_old_cutadapt_file(report):
     report_dir = {}
     try:
         with open(report) as report:
@@ -236,6 +254,95 @@ def parse_cutadapt_file(report):
     except:
             print report
     return report_dir
+
+
+def get_number_and_percent(line):
+    line = [x.strip() for x in line.strip().split(":")]
+    line = [line[0]] + line[1].split()
+    line[2] = float(line[2][1:-2])
+    line[1] = int(line[1].replace(",", ""))
+    return line
+
+def get_number(line):
+    line = [x.strip() for x in line.strip().split(":")]
+    line[1] = int(line[1].replace(",", ""))
+    return line
+
+def strip_bp(line):
+    return line.replace("bp", "")
+
+def remove_header(file_handle):
+    """ for both SE and PE output removes header unifromly from cutadapt metrics file"""
+    file_handle.next()
+    file_handle.next()
+    file_handle.next()
+    file_handle.next()
+    file_handle.next()
+    file_handle.next()
+    file_handle.next()
+    #print foo.next()
+
+def parse_new_cutadapt_file(report):
+    report_dict = {}
+    try:
+        with open(report) as file_handle:
+            remove_header(file_handle)
+            processed_reads = get_number(file_handle.next())
+            paired_file = processed_reads[0] == 'Total read pairs processed'
+            if paired_file:
+                r1_adapter = get_number_and_percent(file_handle.next())
+                r2_adapter = get_number_and_percent(file_handle.next())
+            else:
+                adapter = get_number_and_percent(file_handle.next())
+
+            too_short = get_number_and_percent(file_handle.next())
+            written = get_number_and_percent(file_handle.next())
+            file_handle.next()
+
+            bp_processed = get_number(strip_bp(file_handle.next()))
+            if paired_file:
+                r1_bp_processed = get_number(strip_bp(file_handle.next()))
+                r2_bp_processed = get_number(strip_bp(file_handle.next()))
+
+            bp_quality_trimmed = get_number_and_percent(strip_bp(file_handle.next()))
+            if paired_file:
+                r1_bp_trimmed = get_number(strip_bp(file_handle.next()))
+                r2_bp_trimmed = get_number(strip_bp(file_handle.next()))
+
+            bp_written = get_number_and_percent(strip_bp(file_handle.next()))
+            if paired_file:
+                r1_bp_written = get_number(strip_bp(file_handle.next()))
+                r2_bp_written = get_number(strip_bp(file_handle.next()))
+
+    except Exception as e:
+        print e
+        print report
+
+    report_dict['Processed reads'] = processed_reads[1]
+    if paired_file:
+        report_dict["Read 1 with adapter"] = r1_adapter[1]
+        report_dict["Read 1 with adapter percent"] = r1_adapter[2]
+        report_dict["Read 2 with adapter"] = r2_adapter[1]
+        report_dict["Read 2 with adapter percent"] = r2_adapter[2]
+        report_dict['Read 1 basepairs processed'] = r1_bp_processed[1]
+        report_dict['Read 2 basepairs processed'] = r2_bp_processed[1]
+        report_dict['Read 1 Quality-trimmed'] = r1_bp_trimmed[1]
+        report_dict['Read 2 Quality-trimmed'] = r2_bp_trimmed[1]
+        report_dict['Read 1 {}'.format(bp_written[0])] = r1_bp_written[1]
+        report_dict['Read 2 {}'.format(bp_written[0])] = r2_bp_written[1]
+
+
+    report_dict['Reads that were too short'] = too_short[1]
+    report_dict['Reads that were too short percent'] = too_short[2]
+    report_dict['Reads Written'] = written[1]
+    report_dict['Reads Written perccent'] = written[2]
+    report_dict['Processed bases'] = bp_processed[1]
+    report_dict['Quality-trimmed'] = bp_quality_trimmed[1]
+    report_dict['Quality-trimmed percent'] = bp_quality_trimmed[2]
+    report_dict[bp_written[0]] = bp_written[1]
+    report_dict["{} percent".format(bp_written[0])] = bp_written[2]
+
+    return report_dict
 
 def commas(x):
     try:
