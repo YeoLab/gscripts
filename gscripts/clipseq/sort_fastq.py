f8acbe01d73184cd63fb350321c16e5415cba31e
Gabriel Pratt <gpratt@ucsd.edu>
2015-07-02 13:05:57 -0700

    fixed sort_fastq to use a temp file, which will prevent issues when running stuff in parallel

--- gscripts/clipseq/sort_fastq.py
+++ gscripts/clipseq/sort_fastq.py
@@ -2,10 +2,11 @@ __author__ = 'gpratt'
 import argparse
 
 import subprocess
-
+import tempfile
 
 def sort_fastq_inplace(fastq, out_fastq):
-    call = "cat {0} | sed  's/\t/ /' | paste  - - - - | sort -k1,1 -S 3G | tr '\t' '\n' > foo.fq && mv foo.fq {1}".format(fastq, out_fastq)
+    handle, tmp_file = tempfile.mkstemp()
+    call = "cat {0} | sed  's/\t/ /' | paste  - - - - | sort -k1,1 -S 3G | tr '\t' '\n' > {2} && mv {2} {1}".format(fastq, out_fastq, tmp_file)
     subprocess.call(call, shell=True)
 
 
@@ -19,8 +20,8 @@ if __name__ == "__main__":
     parser.add_argument("--out_fastq_2", help="Sorted Fastq", required=True)
 
     args = parser.parse_args()
-    sort_fastq_inplace((args.inFastq_1, args.outFastq_1))
-    sort_fastq_inplace((args.inFastq_2, args.outFastq_2))
+    sort_fastq_inplace(args.in_fastq_1, args.out_fastq_1)
+    sort_fastq_inplace(args.in_fastq_2, args.out_fastq_2)
 
 
 #If this solution stops working revisit this post
