ae272f34a20dfdac504eca874c3fae97c2d921eb
Gabriel Pratt <gpratt@ucsd.edu>
2016-02-15 19:27:13 -0800

    updated idr scripts to allow for simple replicates

--- gscripts/clipseq/perform_idr.py
+++ gscripts/clipseq/perform_idr.py
@@ -24,38 +24,52 @@ class IDR():
    
 
 
+def pre_process_bam(bam):
+    #split bam file into two, return file handle for the two bam files
+    p = subprocess.Popen("samtools view " + bam + " | wc -l", shell=True, stdout=subprocess.PIPE) # Number of reads in the tagAlign file
+    stdout, stderr = p.communicate()
+    nlines = int(stdout) / 2
+    #print "samtools view " + args.bam + " | shuf | split -d -l " + str(nlines) + " - " + args.bam
+    p = subprocess.Popen("samtools view " + bam + " | shuf | split -d -l " + str(nlines) + " - " + bam, shell=True) # This will shuffle the lines in the file and split it into two parts
+    p.wait()
+    p1 = subprocess.Popen("samtools view -H " + bam + " | cat - " + bam + "00 | samtools view -bS - | samtools sort - " + bam + "00.sorted", shell=True)
+    p2 = subprocess.Popen("samtools view -H " + bam + " | cat - " + bam + "01 | samtools view -bS - | samtools sort - " + bam + "01.sorted", shell=True)
+    p1.wait()
+    p2.wait()
+    return bam + "00", bam + "01"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Runs IDR on a given bam file')
     parser.add_argument(
         '--bam', required=True, help='bam file to run IDR on')
+    parser.add_argument('--rep2', required=False, default=None, help='if we want to perform merged pesudoreplicates this is what we run')
+    parser.add_argument('--merged_file_name', help='if using rep2 name the merged file here')
     parser.add_argument('--premRNA', action="store_true", help="if the RBP binds to premrna use this flag")
     parser.add_argument('--species', required=True, help="choose a species, either hg19 or mm9")
     parser.add_argument('--out', required=True, help="outfile name")
     parser.add_argument('--genome', required=True, help="location of genome sizes file")
     parser.add_argument("-method", default="clipper", help="possible idr programs to run on, clipper / piranah")
+    parser.add_argument("--p_value", default=.05, help="p-value cutoff for clipper before IDR")
     args = parser.parse_args() 
    
     premRNA = ""
     if args.premRNA:
 	"--premRNA"
-        
-    p = subprocess.Popen("samtools view " + args.bam + " | wc -l", shell=True, stdout=subprocess.PIPE) # Number of reads in the tagAlign file
-    stdout, stderr = p.communicate()
-    nlines = int(stdout) / 2
-    #print "samtools view " + args.bam + " | shuf | split -d -l " + str(nlines) + " - " + args.bam
-    p = subprocess.Popen("samtools view " + args.bam + " | shuf | split -d -l " + str(nlines) + " - " + args.bam, shell=True) # This will shuffle the lines in the file and split it into two parts
-    p.wait()
-    #print "samtools view -H " + args.bam + " | cat - " + args.bam + "00 | samtools view -bS - | samtools sort - " + args.bam + "00.sorted"
-    p1 = subprocess.Popen("samtools view -H " + args.bam + " | cat - " + args.bam + "00 | samtools view -bS - | samtools sort - " + args.bam + "00.sorted", shell=True)
-    p2 = subprocess.Popen("samtools view -H " + args.bam + " | cat - " + args.bam + "01 | samtools view -bS - | samtools sort - " + args.bam + "01.sorted", shell=True)
-    p1.wait()
-    p2.wait()
+    
+    if args.rep2:
+        process_bam = args.merged_file_name
+        p0 = subprocess.Popen("samtools merge {} {} {} -f".format(process_bam, args.rep2, args.bam), shell=True)
+        p0.wait()
+    else:
+        process_bam = args.bam
 
-    print "done seperating, calling peaks"
+    bam01, bam02 = pre_process_bam(process_bam)
+    print("done seperating, calling peaks")
     if args.method == "clipper":
-        p3 = subprocess.Popen("clipper -b " + args.bam + "00.sorted.bam -s " + args.species + " -o " + args.bam + "00.sorted.peaks.bed --poisson-cutoff=.90 --superlocal --bonferroni --threshold-method binomial " + premRNA, shell=True)
-        p4 = subprocess.Popen("clipper -b " + args.bam + "01.sorted.bam -s " + args.species + " -o " + args.bam + "01.sorted.peaks.bed --poisson-cutoff=.90 --superlocal --bonferroni --threshold-method binomial " + premRNA, shell=True)
+        p3 = subprocess.Popen("clipper -b {}.sorted.bam -s {} -o {}.sorted.peaks.bed --poisson-cutoff={} --superlocal --bonferroni --threshold-method binomial {}".format(bam01, args.species, bam01, args.p_value, premRNA), shell=True)
+        p4 = subprocess.Popen("clipper -b {}.sorted.bam -s {} -o {}.sorted.peaks.bed --poisson-cutoff={} --superlocal --bonferroni --threshold-method binomial {}".format(bam02, args.species, bam02, args.p_value, premRNA), shell=True)
         p3.wait()
         p4.wait()
         
@@ -75,8 +89,8 @@ if __name__ == "__main__":
         p4.wait()
 
     idr = IDR()
-    idr.clipper_idr(args.bam + "00.sorted.peaks.bed", args.bam + "01.sorted.peaks.bed", args.out, args.genome)
-    print "reformatting and performing IDR"
+    idr.clipper_idr(process_bam + "00.sorted.peaks.bed", process_bam + "01.sorted.peaks.bed", args.out, args.genome)
+    print("reformatting and performing IDR")
     
 
     
