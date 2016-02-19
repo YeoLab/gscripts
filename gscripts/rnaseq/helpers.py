2db62554c69c0b822ebc8f4f5bc89d8523e2e1b3
gpratt <gpratt@gmail.com>
2016-02-19 14:17:45 -0800

    included read density helpers in rna-seq package

--- gscripts/rnaseq/helpers.py
+++ gscripts/rnaseq/helpers.py
@@ -1,5 +1,7 @@
 __author__ = 'gpratt'
 import pandas as pd
+import pyBigWig
+
 def counts_to_rpkm(featureCountsTable):
     """
     Given a dataframe or a text file from featureCounts and converts that thing into a dataframe of RPKMs
@@ -11,3 +13,17 @@ def counts_to_rpkm(featureCountsTable):
     lengths = featureCountsTable['Length']
     mapped_reads = counts.sum()
     return (counts * pow(10, 9)).div(mapped_reads, axis=1).div(lengths, axis=0)
+
+
+class ReadDensity():
+    def __init__(self, pos, neg):
+        self.pos = pyBigWig.open(pos)
+        self.neg = pyBigWig.open(neg)
+
+    def values(self, chrom, start, end, strand):
+        if strand == "+":
+            return self.pos.values(chrom, start, end)
+        elif strand == "-":
+            return list(reversed(self.neg.values(chrom, start, end)))
+        else:
+            raise("Strand neither + or -")
